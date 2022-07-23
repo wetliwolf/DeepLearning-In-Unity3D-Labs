612819001e8384c16fe0adc9c838fbdd05109e22
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2022-07-23 12:58:31 +0000

    v2.5.0

--- Assets/AI/Perceptron.cs
+++ Assets/AI/Perceptron.cs
@@ -10,4 +10,54 @@ namespace GeneticAlgorithm
 
         public Perceptron(sbyte[] weights, sbyte bias)
         {
-            // Initialize perceptron
+            // Initialize perceptron with weights and bias
+            this.weights = new double[weights.Length];
+            for (int i = 0; i < weights.Length; i++)
+                this.weights[i] = weights[i] / (double)128;
+            this.bias = bias / (double)128;
+        }
+
+        // Main perceptron elaborator
+        public void execute_perceptron(double[] input)
+        {
+            output = sigmoid(dot_product(input, weights) + bias);
+        }
+
+        public double getOutput()
+        {
+            return output;
+        }
+
+        public double getWeight(int wId)
+        {
+            return weights[wId];
+        }
+
+        // Dot product
+        private double dot_product(double[] vector1, double[] vector2)
+        {
+            double result = 0;
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                result += vector1[i] * vector2[i];
+            }
+            return result;
+        }
+
+        // Activator functions
+        private double sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+        private int binary(double x)
+        {
+            if (x >= 0) return 1;
+            else return 0;
+        }
+        private double tanh(double x)
+        {
+            return Math.Tanh(x);
+        }
+
+    }
+}
---
+++ Assets/Agent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GeneticAlgorithm;
+using System.Threading;
+
+public class Agent : MonoBehaviour {
+
+    const float multiplier = .3f;
+    public float speed = 0, rotate = 0;
+    Rigidbody rigidbody;
+    public GameObject eye_sensor;
+    public Material neuronMaterial;
+    RaycastHit hit;
+    public sbyte[] genome;
+
+    double health = 1, startTime, points = 0;
+
+    public int genomeId = -1;
+    public GeneticController gController;
+    public double[] inputs, outputs;
+    public double fitness;
+
+    public LayerMask lm;
+
+    const int rays = 20;
+    const float deg = 120;
+
+    void Start() {
+        //Physics.IgnoreLayerCollision(8, 8, true);
+        rigidbody = gameObject.AddComponent<Rigidbody>();
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        startTime = Time.time;
+        inputs = new double[rays * 3];
+        outputs = new double[1];
+
+        if (genomeId == 0) gController.printANetwork(genomeId, neuronMaterial);
+    }
+
+    void Update() {
+        
+        for (int i = 0; i < rays * 3; i += 3)
+        {
+            if (Physics.Raycast(eye_sensor.transform.position, Quaternion.Euler(0, -deg / 2 + i * (deg / rays) / 2, 0) * transform.forward, out hit, 15, lm))
+            {
+                Color color = Color.red;
+
+                if (hit.transform.tag == "point")
+                {
+                    inputs[i] = double.Parse(hit.transform.name) / 2;
+                    inputs[i + 1] = 0;
+                    inputs[i + 2] = 0;
+                    color = Color.green;
+                }
+                else if (hit.transform.tag == "wall")
+                {
+ 
