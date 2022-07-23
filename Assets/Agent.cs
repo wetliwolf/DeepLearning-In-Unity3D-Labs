26bf372509d687c3171108313ac4292c60eca6e8
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2022-07-23 16:20:24 +0000

    Improve changelog

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -54,4 +54,52 @@ public class Agent : MonoBehaviour {
                 }
                 else if (hit.transform.tag == "wall")
                 {
- 
+                    inputs[i] = 0;
+                    inputs[i + 1] = hit.distance / 15;
+                    inputs[i + 2] = 0;
+                }
+                else if (hit.transform.tag == "agent")
+                {
+                    inputs[i] = 0;
+                    inputs[i + 1] = 0;
+                    inputs[i + 2] = hit.distance / 50;
+                    color = Color.blue;
+                }
+
+
+                Debug.DrawLine(eye_sensor.transform.position, hit.point, color);
+            }
+            else
+            {
+                inputs[i] = 0;
+                inputs[i + 1] = 0;
+            }
+        }
+
+        outputs = gController.executeGenome(genomeId, inputs);
+        speed = (float)outputs[0];
+        rotate = (float)outputs[1];
+
+        transform.position += speed * 10 * transform.forward * Time.deltaTime;
+        transform.Rotate(0, 50 * (rotate - .5f), 0);
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        points += double.Parse(coll.gameObject.name);
+        Destroy(coll.gameObject);
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "wall")
+        {
+            health -= .2;
+        }
+    }
+
+    public void end()
+    {
+        fitness = 5 * points + 5 * health;
+    }
+}
